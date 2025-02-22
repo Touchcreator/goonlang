9f26429391e66b652cf864120bb28b97ecfee010
Touchcreator <tochiokl62@gmail.com>
2025-02-21 19:48:19 -0500

    first commit does it even work

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
 from lark import Lark
-
-parser = Lark.open("goonlang.lark")
+from goonlang.parser import Parser
 
 goonlang_code = """print("gooning");print("goon");"""
 
-def run_instruction(inst):
-    if inst.data == "funcstate":
-        print(inst.children)
-
-
-def run_goonlang(program):
-    parse_tree = parser.parse(program)
- 
-    for inst in parse_tree.children:
-        run_instruction(inst)
-
-
-run_goonlang(goonlang_code)
+parser = Parser(goonlang_code, "goonlang.lark")
